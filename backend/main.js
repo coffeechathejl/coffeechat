9713b969717f19c04306e88f2160413a63327db9
Hashir <50779880+Hashir103@users.noreply.github.com>
2024-02-03 00:01:39 -0500

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -1,17 +1,36 @@
 const http = require("http");
 const express = require("express");
 const { spawn } = require("child_process");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const hostname = "127.0.0.1";
 const port = 3000;
+const uri = process.env.MONGO_URI;
 
 const app = express();
+
+async function insertDocument(documentToInsert) {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+      await client.connect();
+      const database = client.db("users");
+      const collection = database.collection("students");
+      const result = await collection.insertOne(documentToInsert);
+
+      console.log(`Document inserted with _id: ${result.insertedId}`);
+      return result;
+  } finally {
+      await client.close();
+  }
+}
 
 app.get("/", (req, res) => {
   res.status(200).send();
 });
 
-app.get("/createProfileEntry/:name", (req, res) => {
+app.get("/createProfileEntry/:name", async (req, res) => {
   const name = req.params.name;
 
   // Spawn the Python script with arguments
@@ -26,13 +45,28 @@ app.get("/createProfileEntry/:name", (req, res) => {
   });
 
   // Handle Python script completion
-  pythonProcess.on("close", (code) => {
+  pythonProcess.on("close", async (code) => {
     if (code === 0) {
       // convert responseData to JSON
       responseData = JSON.parse(responseData);
 
       res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(responseData));
+
+      try {
+        const documentToInsert = responseData;
+
+        if (!documentToInsert) {
+            return res.status(400).json({ error: 'Bad Request. Missing document data.' });
+        }
+
+        const result = await insertDocument(documentToInsert);
+
+        res.status(201).json({ message: 'Document inserted successfully', insertedId: result.insertedId });
+
+    } catch (error) {
+        console.error('Error inserting document:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
     } else {
       res.status(500).send(`Error executing Python script. Exit code: ${code}`);
     }
