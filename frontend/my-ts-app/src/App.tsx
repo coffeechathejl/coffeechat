73cbe804c2e8d787a0068aae57368c0e05a2c1cb
ethanwhitcher <ewhitcher123@gmail.com>
2024-02-03 17:40:02 -0500

    implemented react router

--- frontend/my-ts-app/src/App.tsx
+++ frontend/my-ts-app/src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import HomePage from './pages/homepage'
+import HelloWorld from './pages/helloworld';
 
 function App() {
 
   return (
-    <>
-      <div>
-        "Hello World"
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage onClick={() => console.log('Clicked!')} />} />
+      <Route path="/helloworld" element={<HelloWorld />} />
+    </Routes>
+
+
   )
 }
 
