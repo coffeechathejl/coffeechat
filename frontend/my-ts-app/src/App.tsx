aa615f4c4fc227fb0571f39e078c207590377266
laurenlidhar <98188715+laurenlidhar@users.noreply.github.com>
2024-02-02 23:20:05 -0500

    set up frontend

---
+++ frontend/my-ts-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        "Hello World"
+      </div>
+    </>
+  )
+}
+
+export default App
