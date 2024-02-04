4a511fa9d2495af03aa8c56ea78d24830e88fec1
ethanwhitcher <ewhitcher123@gmail.com>
2024-02-04 06:23:39 -0500

    finished mentor page

--- frontend/my-ts-app/src/pages/mentorprofile.tsx
+++ frontend/my-ts-app/src/pages/mentorprofile.tsx
@@ -6,30 +6,37 @@ const MentorProfile = () => {
     
     return(
         <div className="container">
-            <div className="column1 column">
+=            <div className="column1 column">
                 <div className="personal-profile">
+                    <div className='left-panel'>
                     <div className="profile-picture">
                         <img src={imageURL} alt="LinkedIn Profile" />
                     </div>
                     <div className="personal-profile-text">
                         <h1>Lauren Lidhar</h1>
-                        <h3>Bio: CS & Queen's</h3>
+                        <h3>Bio: CS @ Queen's</h3>
                         <br />
                         <h2>My Contact Info</h2>
-                        <h4>Email</h4>
-                        <h4>Phone #</h4>
-                        <h4>LinkedIn URL</h4>
-                        <h4>Github</h4>
-                        <h4>Resume</h4>
+                        <h3>Email</h3>
+                        <h3>Phone #</h3>
+                        <h3>LinkedIn URL</h3>
+                        <h3>Github</h3>
+                        <h3>Resume</h3>
                         <br/>
                         <h2>Preferences</h2>
+                        <h3>Tech Industry</h3>
+                    </div>
                     </div>
                 </div>
             </div>
-
             <div className="column2 column">
                 <div className='mentor-info'>
-                    <h1 style={{marginBottom: '0.7em'}}>Meet your Mocha Mentor!</h1>
+                    <div mentor-header>
+                    <div className="centered-div" style={{ textAlign: 'center', marginBottom: '1em', borderBottom: '1px solid black' }}>
+                        <h1>Strong Match</h1>
+                    </div>
+                        <h1 style={{marginBottom: '0.7em'}}>Meet your Mocha Mentor!</h1>
+                    </div>
                     <div className='mentor-content'>
                         <div className='mentor-content-image'>
                             <img src={imageURL} alt="LinkedIn Profile" />
@@ -45,18 +52,29 @@ const MentorProfile = () => {
                 </div>
                 <div className='mentor-experience'>
                     <div className='mentor-experience-content'>
-                        <h1>Experience</h1>
+                        <h1 style={{ marginTop: '0.5em'}}>Experience</h1>
                         <div className='experience-info'>
                             <div className='job-picture'>
                                 <img src={jobURL} alt="Job image"></img>
                             </div>
                             <div className='job-info'>
-                                <h5>Software Developer</h5>
-                                <h5>IBM</h5>
-                                <h5>May 2019 - Present</h5>
+                                <h4>Orientation Chair</h4>
+                                <h4>Queen's Computing Orientation</h4>
+                                <h4>May 2019 - Present</h4>
                             </div>
-
                         </div>
+                        <h1>Education</h1>
+                        <div className='experience-info'>
+                            <div className='job-picture'>
+                                <img src={jobURL} alt="Job image"></img>
+                            </div>
+                            <div className='job-info'>
+                                <h4>Orientation Chair</h4>
+                                <h4>Queen's Computing Orientation</h4>
+                                <h4>May 2019 - Present</h4>
+                            </div>
+                        </div>
+
 
 
 
